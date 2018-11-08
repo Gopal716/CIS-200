76eb63b4e829ce9a7411cceaca03ffcfb774fef3
Gopal716 <35580839+Gopal716@users.noreply.github.com>
2018-11-08 16:56:36 -0500

    Add files via upload

---
+++ Program 4/Prog1B/Prog1/DescendingCopyrightYearOrder.cs
@@ -0,0 +1,32 @@
+﻿// Program 4
+// CIS-200-01
+// Due: 4/19/2017
+// D1924
+
+
+// This class provides an IComparer for the LibraryItem class
+// that orders the objects in descending CopyrightYear order.
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LibraryItems
+{
+   public class DescendingCopyrightYearOrder : Comparer<LibraryItem>
+    {
+        // Precondition:  None
+        // Postcondition: Reverses natural LibraryItem order, descending by CopyrightYear
+        public override int Compare(LibraryItem x, LibraryItem y)
+        {
+          
+
+            return (-1) * x.CopyrightYear.CompareTo(y.CopyrightYear); // Reverses natural order, so descending
+
+
+        }
+
+    }
+}
